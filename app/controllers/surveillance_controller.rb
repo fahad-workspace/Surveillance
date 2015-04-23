87ba8ed4a6f0c6b3eb9511d69091974aaff5ea6a
Fahad Sarwar <fahad.workspace@outlook.com>
2015-04-23 14:48:12 +0530

    Login via GitHub

--- app/controllers/surveillance_controller.rb
+++ app/controllers/surveillance_controller.rb
@@ -1,3 +1,5 @@
+require 'net/http'
+
 class SurveillanceController < ApplicationController
 
   def monitor
@@ -9,7 +11,24 @@ class SurveillanceController < ApplicationController
       repo_name = repo_name.split('.')[0]
       puts user_name
       puts repo_name
+      repo_list = Github.repos.list user: user_name
+      puts "Repo Count: #{repo_list.count}"
+      repo_list.each do |repo|
+        puts repo.name
+      end
+      issues = Github::Client::Issues.new
+      issues.list user: user_name, repo: repo_name, state: 'open' do |issue|
+        issue.labels.each do |name|
+          puts name.name
+        end
+      end
     end
+  end
+  
+  def auth 
+    github = Github.new client_id: 'ab0f000ed4fb6f13a6c6', client_secret: '049d5ef30578afac88a79ea056e17ee23d2b5a41', scope: 'repo'
+    github.authorize_url scope: 'repo'
+    redirect_to github.authorize_url
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  match '*path' => redirect('/'), via: :get
+  # match '*path' => redirect('/'), via: :get
 
   # You can have the root of your site routed with "root"
   root 'surveillance#index'
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   #   get 'products/:id' => 'catalog#view'
 
   post 'surveillance/monitor' => 'surveillance#monitor'
+  get 'surveillance/auth' => 'surveillance#auth'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
