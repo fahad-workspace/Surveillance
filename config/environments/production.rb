baf3cb63572202e261d3404f8ae690a072c4ad0b
Fahad Sarwar <fahad.workspace@outlook.com>
2015-04-25 20:25:00 +0530

    Minor bug fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,11 +28,11 @@ Rails.application.configure do
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
+  # Disable Rails's static asset server (Apache or nginx will already do this)
+  config.serve_static_files = true
+  # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = true
-
-  # Asset digests allow you to set far-future HTTP expiration dates on all assets,
-  # yet still be able to expire them through the digest params.
+  # Generate digests for assets URLs
   config.assets.digest = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
