e35bebf16f7878ec774846d3862f8b3134aea72c
Fahad Sarwar <fahad.workspace@outlook.com>
2015-05-22 13:52:05 +0530

    Added methods for different functionality

--- app/controllers/surveillance_controller.rb
+++ app/controllers/surveillance_controller.rb
@@ -17,100 +17,12 @@ class SurveillanceController < ApplicationController
   end
 
   def monitor
-    # eg : https://github.com/fahad-workspace/Surveillance
-    link = params[:repo]
-    if link =~ /https:\/\/github.com\/(.*)\/(.*)/
-      @user_login = link.split('/')[3]
-      @repo_name = link.split('/')[4]
-      @repo_name.gsub! '.git', ''
-      @full_repo_name = @user_login + '/' + @repo_name
-      begin
-        @repo_list = Octokit.repositories(@user_login)
-        @issuexdata = Array.new
-        @issueydata = Array.new
-        @repo_list.each do |repo|
-          if (repo.open_issues_count > 0)
-            @issuexdata.push(repo.name)
-            @issueydata.push(repo.open_issues_count)
-          end
-        end
-        repo = Octokit.repository(@full_repo_name)
-        @user = User.find_or_create_by(:github_user_id => repo.owner.id, :github_user_login => repo.owner.login, :github_user_type => repo.owner.type)
-        @repository = Repository.find_or_create_by(:github_repository_id => repo.id, :name => repo.name, :full_name => repo.full_name, :private => repo.private, :created_at => repo.created_at, :updated_at => repo.updated_at, :pushed_at => repo.pushed_at, :language => repo.language, :has_issues => repo.has_issues, :open_issues_count => repo.open_issues_count, :subscribers_count => repo.subscribers_count, :user_id => @user.id)
-        miles = Octokit.milestones(@full_repo_name)
-        miles.each do |mile|
-          @user = User.find_or_create_by(:github_user_id => mile.creator.id, :github_user_login => mile.creator.login, :github_user_type => mile.creator.type)
-          @repository.milestones.find_or_create_by(:github_milestone_id => mile.id, :number => mile.number, :title => mile.title, :open_issues => mile.open_issues, :closed_issues => mile.closed_issues, :state => mile.state, :created_at => mile.created_at, :updated_at => mile.updated_at, :due_on => mile.due_on, :closed_at => mile.closed_at, :user_id => @user.id)
-        end
-        lbls = Octokit.labels(@full_repo_name)
-        @labeldata = Array.new
-        lbls.each do |lbl|
-          @repository.labels.find_or_create_by(:name => lbl.name, :color => lbl.color)
-        end
-        contribs = Octokit.contributors(@full_repo_name)
-        cmts = Octokit.commits_since(@full_repo_name, (Date.today - 15).to_s)
-        @contribydata = Array.new
-        @contribtotalxdata = Array.new
-        @contribrecentxdata = Array.new
-        contribs.each do |contrib|
-          individual_commit_count = 0
-          cmts.each do |cmt|
-            if contrib.id == cmt.author.id
-              individual_commit_count = individual_commit_count + 1
-            end
-          end
-          if (contrib.contributions > 2 && individual_commit_count > 0)
-            @contribydata.push(contrib.login)
-            @contribtotalxdata.push(contrib.contributions)
-            @contribrecentxdata.push(individual_commit_count)
-          end
-          @user = User.find_or_create_by(:github_user_id => contrib.id, :github_user_login => contrib.login, :github_user_type => contrib.type)
-          @repository.contributors.find_or_create_by(:total_contributions => contrib.contributions, :recent_contributions => individual_commit_count, :user_id => @user.id)
-        end
-        if repo.has_issues == true
-          isus = Octokit.issues(@full_repo_name)
-          isus.each do |isu|
-            assignee_id = nil
-            milestone_id = nil
-            @user = User.find_or_create_by(:github_user_id => isu.user.id, :github_user_login => isu.user.login, :github_user_type => isu.user.type)
-            if !(isu.assignee.nil?)
-              @assignee = User.find_or_create_by(:github_user_id => isu.assignee.id, :github_user_login => isu.assignee.login, :github_user_type => isu.assignee.type)
-              assignee_id = @assignee.id
-            end
-            if !(isu.milestone.nil?)
-              @milestone = @repository.milestones.find_by(:github_milestone_id => isu.milestone.id)
-              milestone_id = @milestone.id
-            end
-            @issue = @repository.issues.find_or_create_by(:github_issue_id => isu.id, :number => isu.number, :title => isu.title, :state => isu.state, :issue_assignee_id => assignee_id, :milestone_id => milestone_id, :created_at => isu.created_at, :updated_at => isu.updated_at, :closed_at => isu.closed_at, :user_id => @user.id)
-            isu.labels.each do |lbl|
-              @label = @repository.labels.find_by(:name => lbl.name, :color => lbl.color)
-              IssueLabel.find_or_create_by(:issue_id => @issue.id, :label_id => @label.id, :repository_id => @repository.id)
-            end
-          end
-        end
-        @labeldata = Array.new
-        @labels = Label.where(:repository_id => @repository.id)
-        @issuelabel = IssueLabel.where(:repository_id => @repository.id)
-        @labels.each do |label|
-          if ((IssueLabel.where(:repository_id => @repository.id, :label_id => label.id)).count > 0)
-            @labeldata.push([label.name, (IssueLabel.where(:repository_id => @repository.id, :label_id => label.id)).count])
-          end
-        end
-
-        if Rails.env == 'production' then
-          @client.subscribe("https://github.com/#{@full_repo_name}/events/push.json", "https://surveillance-site.herokuapp.com/github_webhooks")
-        else
-          if Ngrok::Tunnel.stopped?
-            Ngrok::Tunnel.start(port: 3000)
-          end
-          @client.subscribe("https://github.com/#{@full_repo_name}/events/*.json", "#{Ngrok::Tunnel.ngrok_url}/github_webhooks")
-        end
-
-      rescue => e
-        flash[:error] = e.message
-      end
+    @link = params[:repo]
+    if @link =~ /https:\/\/github.com\/(.*)\/(.*)/
+      init
+      repo_analyze
     else
-      if link.length == 0
+      if @link.length == 0
         flash.now[:error] = 'No repository link provided!'
       else
         flash.now[:error] = 'Invalid repository link provided!'
@@ -152,6 +64,107 @@ class SurveillanceController < ApplicationController
       end
     end
     @client = Octokit::Client.new
+  end
+
+  def ngrok_server
+    if Rails.env == 'production' then
+      @client.subscribe("https://github.com/#{@full_repo_name}/events/push.json", "https://surveillance-site.herokuapp.com/github_webhooks")
+    else
+      if Ngrok::Tunnel.stopped?
+        Ngrok::Tunnel.start(port: 3000)
+      end
+      @client.subscribe("https://github.com/#{@full_repo_name}/events/*.json", "#{Ngrok::Tunnel.ngrok_url}/github_webhooks")
+    end
+  end
+
+  def repo_analyze
+    begin
+      @repo_list = Octokit.repositories(@user_login)
+      @issuexdata = Array.new
+      @issueydata = Array.new
+      @repo_list.each do |repo|
+        if (repo.open_issues_count > 0)
+          @issuexdata.push(repo.name)
+          @issueydata.push(repo.open_issues_count)
+        end
+      end
+      repo = Octokit.repository(@full_repo_name)
+      @user = User.find_or_create_by(:github_user_id => repo.owner.id, :github_user_login => repo.owner.login, :github_user_type => repo.owner.type)
+      @repository = Repository.find_or_create_by(:github_repository_id => repo.id, :name => repo.name, :full_name => repo.full_name, :private => repo.private, :created_at => repo.created_at, :updated_at => repo.updated_at, :pushed_at => repo.pushed_at, :language => repo.language, :has_issues => repo.has_issues, :open_issues_count => repo.open_issues_count, :subscribers_count => repo.subscribers_count, :user_id => @user.id)
+      miles = Octokit.milestones(@full_repo_name)
+      miles.each do |mile|
+        @user = User.find_or_create_by(:github_user_id => mile.creator.id, :github_user_login => mile.creator.login, :github_user_type => mile.creator.type)
+        @repository.milestones.find_or_create_by(:github_milestone_id => mile.id, :number => mile.number, :title => mile.title, :open_issues => mile.open_issues, :closed_issues => mile.closed_issues, :state => mile.state, :created_at => mile.created_at, :updated_at => mile.updated_at, :due_on => mile.due_on, :closed_at => mile.closed_at, :user_id => @user.id)
+      end
+      lbls = Octokit.labels(@full_repo_name)
+      @labeldata = Array.new
+      lbls.each do |lbl|
+        @repository.labels.find_or_create_by(:name => lbl.name, :color => lbl.color)
+      end
+      contribs = Octokit.contributors(@full_repo_name)
+      cmts = Octokit.commits_since(@full_repo_name, (Date.today - 15).to_s)
+      @contribydata = Array.new
+      @contribtotalxdata = Array.new
+      @contribrecentxdata = Array.new
+      contribs.each do |contrib|
+        individual_commit_count = 0
+        cmts.each do |cmt|
+          if contrib.id == cmt.author.id
+            individual_commit_count = individual_commit_count + 1
+          end
+        end
+        if (contrib.contributions > 2 && individual_commit_count > 0)
+          @contribydata.push(contrib.login)
+          @contribtotalxdata.push(contrib.contributions)
+          @contribrecentxdata.push(individual_commit_count)
+        end
+        @user = User.find_or_create_by(:github_user_id => contrib.id, :github_user_login => contrib.login, :github_user_type => contrib.type)
+        @repository.contributors.find_or_create_by(:total_contributions => contrib.contributions, :recent_contributions => individual_commit_count, :user_id => @user.id)
+      end
+      if repo.has_issues == true
+        isus = Octokit.issues(@full_repo_name)
+        isus.each do |isu|
+          assignee_id = nil
+          milestone_id = nil
+          @user = User.find_or_create_by(:github_user_id => isu.user.id, :github_user_login => isu.user.login, :github_user_type => isu.user.type)
+          if !(isu.assignee.nil?)
+            @assignee = User.find_or_create_by(:github_user_id => isu.assignee.id, :github_user_login => isu.assignee.login, :github_user_type => isu.assignee.type)
+            assignee_id = @assignee.id
+          end
+          if !(isu.milestone.nil?)
+            @milestone = @repository.milestones.find_by(:github_milestone_id => isu.milestone.id)
+            milestone_id = @milestone.id
+          end
+          @issue = @repository.issues.find_or_create_by(:github_issue_id => isu.id, :number => isu.number, :title => isu.title, :state => isu.state, :issue_assignee_id => assignee_id, :milestone_id => milestone_id, :created_at => isu.created_at, :updated_at => isu.updated_at, :closed_at => isu.closed_at, :user_id => @user.id)
+          isu.labels.each do |lbl|
+            @label = @repository.labels.find_by(:name => lbl.name, :color => lbl.color)
+            IssueLabel.find_or_create_by(:issue_id => @issue.id, :label_id => @label.id, :repository_id => @repository.id)
+          end
+        end
+      end
+      @labeldata = Array.new
+      @labels = Label.where(:repository_id => @repository.id)
+      @issuelabel = IssueLabel.where(:repository_id => @repository.id)
+      @labels.each do |label|
+        if ((IssueLabel.where(:repository_id => @repository.id, :label_id => label.id)).count > 0)
+          @labeldata.push([label.name, (IssueLabel.where(:repository_id => @repository.id, :label_id => label.id)).count])
+        end
+      end
+
+      if @client.user.login == @user_login then
+        ngrok_server
+      end
+
+    rescue => e
+      flash[:error] = e.message
+    end
+  end
+
+  def init
+    @user_login = @link.split('/')[3]
+    @repo_name = @link.split('/')[4]
+    @repo_name.gsub! '.git', ''
+    @full_repo_name = @user_login + '/' + @repo_name
   end
 
 end
