0271bbefe67936aa0a0fc86121dbae967ffb1e27
Fahad Sarwar <fahad.workspace@outlook.com>
2015-05-27 14:56:48 +0530

    Updated gems

--- app/controllers/surveillance_controller.rb
+++ app/controllers/surveillance_controller.rb
@@ -127,6 +127,7 @@ class SurveillanceController < ApplicationController
       contribs.each do |contrib|
         individual_commit_count = 0
         cmts.each do |cmt|
+          m
           if contrib.id == cmt.author.id
             individual_commit_count = individual_commit_count + 1
           end
